332f89b3b3c041ff6dab4cea037e6abc07868e9d
Sviatoslav P <sv9tikez@gmail.com>
2026-01-15 23:43:52 +0200

    fix: add validation for SendChangePlaceStatusEmailMessage

--- service-api/src/main/java/greencity/message/SendChangePlaceStatusEmailMessage.java
+++ service-api/src/main/java/greencity/message/SendChangePlaceStatusEmailMessage.java
@@ -1,6 +1,11 @@
 package greencity.message;
 
 import java.io.Serializable;
+
+import greencity.constant.ValidationConstants;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +16,13 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public final class SendChangePlaceStatusEmailMessage implements Serializable {
+    @NotBlank(message = ValidationConstants.USERNAME_MESSAGE)
     private String authorFirstName;
+    @NotBlank()
     private String placeName;
+    @NotBlank
     private String placeStatus;
+    @NotBlank
+    @Email()
     private String authorEmail;
 }
