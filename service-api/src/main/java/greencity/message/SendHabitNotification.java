24f7fc16997cd0b1addb56b0f674d66e063f6e30
Sviatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2026-01-16 13:14:42 -0500

    Fix email validation to return 400 for invalid email input

--- service-api/src/main/java/greencity/message/SendHabitNotification.java
+++ service-api/src/main/java/greencity/message/SendHabitNotification.java
@@ -1,6 +1,9 @@
 package greencity.message;
 
 import java.io.Serializable;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,9 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SendHabitNotification implements Serializable {
+    @NotBlank
     private String name;
+    @NotBlank
+    @Email
     private String email;
 }
