f6e8adff397f40e68bd0ced220ee6b8531d92506
Rostyslav Kholod <kholod.rostik@gmail.com>
2026-01-20 02:27:03 +0200

    Fix [GET /user] In GET /user - Get User dto by principal (email) from access token response 200 OK - need 403 Forbidden

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -135,7 +135,9 @@ public class SecurityConfig {
                     "/ownSecurity/signIn",
                     "/ownSecurity/updatePassword")
                 .permitAll()
-                .requestMatchers(HttpMethod.GET, USER_LINK,
+                .requestMatchers(HttpMethod.GET, USER_LINK)
+                .hasAnyRole(ADMIN, MODERATOR, EMPLOYEE)
+                .requestMatchers(HttpMethod.GET,
                     "/user/shopping-list-items/habits/{habitId}/shopping-list",
                     "/user/{userId}/{habitId}/custom-shopping-list-items/available",
                     "/user/{userId}/profile/", "/user/isOnline/{userId}/",
