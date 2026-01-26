c9e44c0c46a469ea8c331fa7c1f633222627a47f
Sviatoslav P <sv9tikez@gmail.com>
2026-01-26 04:27:04 +0200

    fix(email-api): removed unnecessary imports

--- service-api/src/main/java/greencity/message/SendChangePlaceStatusEmailMessage.java
+++ service-api/src/main/java/greencity/message/SendChangePlaceStatusEmailMessage.java
@@ -2,10 +2,8 @@ package greencity.message;
 
 import java.io.Serializable;
 
-import greencity.constant.ValidationConstants;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
