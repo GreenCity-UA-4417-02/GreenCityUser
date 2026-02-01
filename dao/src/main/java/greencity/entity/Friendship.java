fb4baa56f402aeb364f242f10ad165b69d92ddaa
Sviatoslav P <sv9tikez@gmail.com>
2026-02-01 14:21:01 +0200

    feat: add lazy loading to avoid leager loading

--- dao/src/main/java/greencity/entity/Friendship.java
+++ dao/src/main/java/greencity/entity/Friendship.java
@@ -20,11 +20,11 @@ public class Friendship {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sender_id", nullable = false)
     private User sender;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
