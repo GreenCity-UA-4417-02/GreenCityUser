8f1497b0efa1ab1c7c350d1fe63ad4742b1e1d12
Sviatoslav P <sv9tikez@gmail.com>
2026-02-01 13:30:03 +0200

    feat: create friendship entity and enum for friendship status

---
+++ dao/src/main/java/greencity/entity/Friendship.java
@@ -0,0 +1,34 @@
+package greencity.entity;
+
+import greencity.enums.FriendshipStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "friendship",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"sender_id", "receiver_id"})
+        }
+)
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private FriendshipStatus status;
+}
