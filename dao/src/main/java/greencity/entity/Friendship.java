7335e95a9138869d986d1b8478de4ed1f98a8d9a
Sviatoslav P <sv9tikez@gmail.com>
2026-02-13 17:41:58 +0200

    refactor: rename sender/receiver to user1/user2

--- dao/src/main/java/greencity/entity/Friendship.java
+++ dao/src/main/java/greencity/entity/Friendship.java
@@ -9,28 +9,29 @@ import lombok.*;
 @EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "friendship")
+@ToString
 public class Friendship {
 
     @EmbeddedId
     private FriendshipId id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("senderId")
-    @JoinColumn(name = "sender_id", nullable = false)
-    private User sender;
+    @MapsId("user1Id")
+    @JoinColumn(name = "user1_id", nullable = false)
+    private User user1;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("receiverId")
-    @JoinColumn(name = "receiver_id", nullable = false)
-    private User receiver;
+    @MapsId("user2Id")
+    @JoinColumn(name = "user2_id", nullable = false)
+    private User user2;
 
-    public Friendship(User sender, User receiver) {
-        this(new FriendshipId(sender.getId(), receiver.getId()), sender, receiver);
+    public Friendship(User user1, User user2) {
+        this(new FriendshipId(user1.getId(), user2.getId()), user1, user2);
     }
 
-    private Friendship(FriendshipId id, User sender, User receiver) {
+    private Friendship(FriendshipId id, User user1, User user2) {
         this.id = id;
-        this.sender = sender;
-        this.receiver = receiver;
+        this.user1 = user1;
+        this.user2 = user2;
     }
 }
--- dao/src/main/java/greencity/entity/FriendshipId.java
+++ dao/src/main/java/greencity/entity/FriendshipId.java
@@ -2,20 +2,20 @@ package greencity.entity;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @EqualsAndHashCode
 @Embeddable
 public class FriendshipId implements Serializable {
-    @Column(name = "sender_id")
-    private Long senderId;
+    @Column(name = "user1_id")
+    private Long user1Id;
 
-    @Column(name = "receiver_id")
-    private Long receiverId;
+    @Column(name = "user2_id")
+    private Long user2Id;
 }
